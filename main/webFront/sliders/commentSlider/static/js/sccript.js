04c85e8d6810023fe56320c9467ace35ede8e84a
Hamid Azimi <hamidazimi.azp@gmail.com>
2021-12-04 14:44:28 +0330

    add new project to sldiers section -- comment slider

---
+++ main/webFront/sliders/commentSlider/static/js/sccript.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop : true,
+});
