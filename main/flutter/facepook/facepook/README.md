1f04400ef56710af8b8f48fed7b69acdd97a6f4b
Hamid Azimi <hamidazimi.azp@gmail.com>
2022-02-05 13:20:51 +0330

    add facebook and netflix ui with flutter

---
+++ main/flutter/facepook/facepook/README.md
@@ -0,0 +1,3 @@
+# facepook UI
+
+https://user-images.githubusercontent.com/89732313/144842293-90f0f00f-0d00-494a-a8ff-4c7b5e077cb1.mp4
---
+++ main/flutter/facepook/facepook/android/app/src/main/kotlin/com/example/facepook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.facepook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
