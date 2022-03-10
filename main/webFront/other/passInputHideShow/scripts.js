0143b8a2ce9e7d18bfaf164b86e0600b4e0ed8db
Hamid Azimi <hamidazimi.azp@gmail.com>
2022-03-10 11:14:26 +0330

    Improve the script in the passinputhideshow project in other section

--- main/webFront/other/passInputHideShow/scripts.js
+++ main/webFront/other/passInputHideShow/scripts.js
@@ -5,13 +5,10 @@ const showHiddenInput = (iputOverlay, inputPass, inputIcon) => {
     eyeIcon.addEventListener("click", () => {
         if (input.type === "password") {
             input.type = "text";
-            eyeIcon.classList.remove("fa-regular", "fa-eye-slash");
-            eyeIcon.classList.add("fa", "fa-eye");
         } else {
             input.type = "password";
-            eyeIcon.classList.remove("fa", "fa-eye");
-            eyeIcon.classList.add("fa-regular", "fa-eye-slash");
         }
+        eyeIcon.classList.toggle("fa", "fa-eye", "fa-regular", "fa-eye-slash");
         overlay.classList.toggle("show");
     });
 }
