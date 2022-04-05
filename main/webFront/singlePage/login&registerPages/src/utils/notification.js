9f9412c05ee6807dc0d5f2c5a3838853a7a666ef
Hamid Azimi <hamidazimi.azp@gmail.com>
2022-04-05 15:09:37 +0430

    remove login&register project

--- main/webFront/singlePage/login&registerPages/src/utils/notification.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-
-export const notify = (text, type) => {
-    if (type === "success") {
-        toast.success(text);
-    } else {
-        toast.error(text);
-    }
-}
